b8202323e723e5cef141c532bf9b1573b4dbf237
MD SALIM <mdsalim.iiitr@gmail.com>
2023-07-17 12:18:04 +0530

    First Commit - Implemented simple working of react redux

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Counter from "./components/Count";
+import {useDispatch} from "react-redux";
+
+function App() {
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <button onClick={(e) => dispatch({type: "INCREMENT"})}>Increment</button>
+      <Counter/>
+      <button onClick={(e) => dispatch({type: "DECREMENT"})}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
